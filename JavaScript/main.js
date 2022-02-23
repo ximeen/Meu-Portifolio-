a5ee0ded17059cfee7f9b90b03af9916e7efebe5
Monge1331 <gabriximenes100@gmail.com>
2022-02-22 21:20:13 -0300

    :package: Funcionalidade do menu lateral

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,7 @@
-import './style.css'
+let hamburguer = document.querySelector(".hamburguer");
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+hamburguer.addEventListener("click", function(){
+ var sideBar = document.querySelector(".container");
+ sideBar.classList.toggle("ShowMenu");
+
+});
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Meu-Portifolio-
+
 Meu portfólio de desenvolvedor 
 
 ## Tarefas 
+
   O controle das tarefas desse projeto será realizada no gitHub
 
 ## Icones 
 
-:package: Nova funcionalidade
-:up: Atualização
-:bug: Correção de bugs
-:checkered_flag: release
+ - :package: Nova funcionalidade
+ - :up: Atualização
+ - :beetle: Correção de bugs
+ - :checkered_flag: release
